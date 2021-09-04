9bee56d0b2d33c87c2951b2deede58fabe0dfd62
Ahamed02 <90087321+Ahamed02@users.noreply.github.com>
2021-09-04 14:29:21 +0530

    Assign.1

---
+++ Assign.java
@@ -0,0 +1,11 @@
+
+public class Assign {
+	public static void main(String[]args) {
+		System.out.println("Start initializing");
+		int a=6;
+		float f=6.5f;
+		char myChar='A';
+		double myDouble=256.78746;
+		boolean Bool=false;
+	}
+}
---
+++ fourth.java
@@ -0,0 +1,10 @@
+
+public class fourth {
+	public static void main(String[]args) {
+		System.out.println("\nJava Version: "+System.getProperty("java.version"));
+		System.out.println("\nJava Runtime Version: "+System.getProperty("java.runtime.version"));
+		System.out.println("\nJava Vendor: "+System.getProperty("java.vendor.url"));
+		System.out.println("\nJava Class Path: "+System.getProperty("java.class.path")+"\n'");
+	}
+
+}
